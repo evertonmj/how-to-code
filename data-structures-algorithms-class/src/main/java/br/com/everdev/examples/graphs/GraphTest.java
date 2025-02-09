6a1d9cbd8daa4a698ebfbae7517304d7c43169ce
Everton Mendonca Jesus <evertonmj@gmail.com>
2025-02-09 10:35:23 -0300

    graphs

---
+++ data-structures-algorithms-class/src/main/java/br/com/everdev/examples/graphs/GraphTest.java
@@ -0,0 +1,31 @@
+package br.com.everdev.examples.graphs;
+
+import static br.com.everdev.examples.utils.IOUtils.printn;
+
+public class GraphTest {
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+
+        graph.addVertex("camacari");
+        graph.addVertex("salvador");
+        graph.addVertex("dias davila");
+        graph.addVertex("simoes filho");
+
+        graph.addEdge("camacari", "salvador");
+        graph.addEdge("camacari", "dias davila");
+        graph.addEdge("camacari", "simoes filho");
+        graph.addEdge("salvador", "simoes filho");
+
+        printn("add edges");
+        graph.print();
+
+        graph.removeEdge("camacari", "simoes filho");
+        printn("remove edges");
+        graph.print();
+
+        graph.removeVertex("salvador");
+        printn("remove vertex");
+        graph.print();
+    }
+}
